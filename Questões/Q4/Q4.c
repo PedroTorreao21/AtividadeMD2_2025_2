d5e8f0a9a24d1a96a56bde57ab79b1122a0ce830
Pedro Victor Lima Torreão <pedrovtorreao@gmail.com>
2025-10-17 21:20:23 -0300

    Q4c
    Q4c

---
+++ Questões/Q4/Q4.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+
+int mdcComPassos(int a, int b){
+    int resto;
+    while ([1] != 0){
+        resto = a % b;
+        printf("Algoritmo de Euclides: %d mod %d = %d \n", a, b, resto);
+        a = b;
+        b = [2];
+    }
+    return a;
+}
+int inversoModular(int a, int m){
+    int m0 = m, t, q;
+    int x0 = 0, x1 = 1;
+    int A = a, int B = m;
+
+    [3](a, m);
+
+    while(m != 0) {
+        q = a/m;
+        t = m;
+        m = a % m;
+        a = t;
+
+        t = x0;
+        x0 = x1 - q * x0;
+        x1 = t;
+    }
+    if (x1 < 0)
+        [4] += m0;
+    printf("\nSubstituindo, temos que o inverso de %d em %d é %d.\n\n", A, B, x1);
+    return x1;
+}
+
+int powMod(int base, int exp, int mod){
+    long long res = 1;
+    long long base = b % mod;
+    while(exp > 0){
+        if([5])
+            res = (res * b) % mod;
+        b = (b * b) % mod;
+        exp >>= 1;
+    }
+    return (int)res;
+}
+
+int main(){
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+
+    int H, G, Zn, x, n1;
+
+    printf("Insira H: ");
+    scanf("%d", &H);
+    printf("Insira G: ");
+    scanf("%d", &G);
+    printf("Insira Zn: ");
+    scanf("%d", &Zn);
+    printf("Insira x: ");
+    scanf("%d", &x);
+    printf("Insira n1: ");
+    scanf("%d", &n1);
+    printf("\n");
+
+    int inverso = [6](G, Zn);
+    int a = (H * inverso) % Zn;
+
+    printf("Fazendo a multiplicação modular: %d * %d mod %d cong %d\n", H, inverso, Zn, a);
+    printf("Sendo %d o inverso de %d\n", inverso, G);
+
+    int resultado = [7](a, x, n1);
+    printf("Valor final da congruencia: %d\n", resultado);
+
+    return 0;
+}
